82cf8ab3ba3feac92bc1b04550588f1f13b635d9
Matheus Pereira Dos Santos <50374464812@info.scs>
2022-03-30 11:19:06 -0300

    Introdução e login termimados

--- src/screens/ListaRotas.js
+++ src/screens/ListaRotas.js
@@ -2,12 +2,48 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity, Touchable } from 'react-native';
 import { useNavigate } from 'react-router';
 
+import AppLoading from 'expo-app-loading';
+import { useFonts, Sen_400Regular, Sen_800ExtraBold, Sen_700Bold } from '@expo-google-fonts/sen';
+
+
+
+import {
+    FlatList,
+    SafeAreaView,
+    ScrollView,
+    useColorScheme,
+    ImageBackground,
+    Image,
+    TextInput,
+} from 'react-native';
+
 export default function ListaRotas({ navigation }) {
+
+
+    let [fontsLoaded] = useFonts({
+        Sen_700Bold,
+        Sen_400Regular
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
+
 
     return (
         <View style={styles.container}>
-            <Text>Lista de rotas</Text>
+
+            <Image source={require('../assets/caminhaozinho.png')}
+                style={styles.MainImgLogin}
+                />
+            
+            <Text style={styles.TextMain}>Nenhuma rota foi encontrada. Escaneie a placa do veículo para ter acesso a todos os serviços programados</Text>
+
+
+
             <StatusBar style="auto" />
+        
+        
         </View>
     )
 }
@@ -16,7 +52,19 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
+        width: '100%',
+        paddingHorizontal: '4%',
         alignItems: 'center',
         justifyContent: 'center',
     },
-});
+
+    TextMain: {
+        fontFamily: 'Sen_400Regular',
+        fontSize: 16,
+        width: 310,
+        height: 100,
+        color: '#888888',
+        textAlign: 'center',
+    },
+
+})
